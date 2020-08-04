b09a488ebfdacd2b33fef8ce4cd4dba9cfb8b945
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2020-08-04 19:28:20 +0100

    project 1 - complete

--- Projects & Certificates/Data Analysis with Python Projects/Project 1 - Mean-Variance-Standard Deviation Calculator/README.md
+++ Projects & Certificates/Data Analysis with Python Projects/Project 1 - Mean-Variance-Standard Deviation Calculator/README.md
@@ -13,3 +13,8 @@ Create a function that uses Numpy to output the mean, variance, and standard dev
 ```
 
 # Comments & Hurdles
+
+- Overall happy with my first attempt at a NumPy project
+- On first attempt only thing I was missing was .tolist() to satisfy requirement
+- Throwing error based on != 9 covers more than the requirement
+- I could have used .reshape for a better 3,3 matrix
--- Projects & Certificates/Data Analysis with Python Projects/Project 1 - Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Projects & Certificates/Data Analysis with Python Projects/Project 1 - Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -5,33 +5,34 @@ def calculate(list):
     if (len(list) != 9):
         raise ValueError('List must contain nine numbers.')
 
+    # np.reshape(list,(3,3)) also works
     matrix = np.array([list[0:3], list[3:6], list[6:]])
 
-    mean1 = np.mean(matrix, axis=0)
-    mean2 = np.mean(matrix, axis=1)
-    meanflat = np.mean(matrix)
+    mean1 = np.mean(matrix, axis=0).tolist()
+    mean2 = np.mean(matrix, axis=1).tolist()
+    meanflat = np.mean(matrix).tolist()
 
-    var1 = np.var(matrix, axis=0)
-    var2 = np.var(matrix, axis=1)
-    varflat = np.var(matrix)
+    var1 = np.var(matrix, axis=0).tolist()
+    var2 = np.var(matrix, axis=1).tolist()
+    varflat = np.var(matrix).tolist()
 
-    std1 = np.std(matrix, axis=0)
-    std2 = np.std(matrix, axis=1)
-    stdflat = np.std(matrix)
+    std1 = np.std(matrix, axis=0).tolist()
+    std2 = np.std(matrix, axis=1).tolist()
+    stdflat = np.std(matrix).tolist()
 
-    max1 = np.max(matrix, axis=0)
-    max2 = np.max(matrix, axis=1)
-    maxflat = np.max(matrix)
+    max1 = np.max(matrix, axis=0).tolist()
+    max2 = np.max(matrix, axis=1).tolist()
+    maxflat = np.max(matrix).tolist()
 
-    min1 = np.min(matrix, axis=0)
-    min2 = np.min(matrix, axis=1)
-    minflat = np.min(matrix)
+    min1 = np.min(matrix, axis=0).tolist()
+    min2 = np.min(matrix, axis=1).tolist()
+    minflat = np.min(matrix).tolist()
 
-    sum1 = np.sum(matrix, axis=0)
-    sum2 = np.sum(matrix, axis=1)
-    sumflat = np.sum(matrix)
+    sum1 = np.sum(matrix, axis=0).tolist()
+    sum2 = np.sum(matrix, axis=1).tolist()
+    sumflat = np.sum(matrix).tolist()
 
-    return {
+    result = {
         'mean': [mean1, mean2, meanflat],
         'variance': [var1, var2, varflat],
         'standard deviation': [std1, std2, stdflat],
@@ -39,3 +40,5 @@ def calculate(list):
         'min': [min1, min2, minflat],
         'sum': [sum1, sum2, sumflat]
     }
+
+    return result
